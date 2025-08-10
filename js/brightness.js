ede058286eb65172c511379e723465570361bd12
SCRIPTERS-HUB-IOS <testaccmcc3@gmail.com>
2025-08-10 13:57:57 +0800

    Delete js/brightness.js

--- js/brightness.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const brightnessRange = document.getElementById('brightnessRange');
-  brightnessRange.addEventListener('input', e => {
-    const val = e.target.value;
-    document.documentElement.style.setProperty('--brightness', val / 100);
-    brightnessRange.setAttribute('aria-valuenow', val);
-  });
-});
